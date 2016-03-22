7dc2788967c82862c51d24a0e523db8bf0651c12
Santiago Graviano <santiago.graviano@gmail.com>
2016-03-22 05:58:53 -0300

    Directory structure update

--- src/Facades/MP.php
+++ src/Facades/MP.php
@@ -4,14 +4,10 @@ namespace SantiGraviano\LaravelMercadoPago\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
-* Mercado Pago SDK Facade
-*/
 class MP extends Facade
 {
-	protected static function getFacadeAccessor()
-	{
-		return 'MP';
-	}
+    protected static function getFacadeAccessor()
+    {
+        return 'MP';
+    }
 }
-
