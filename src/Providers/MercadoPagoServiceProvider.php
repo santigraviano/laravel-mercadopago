903060811d9761d1e84009e60a9c6c4dbea1c692
Gabriel <eu@gabrielf.com>
2017-04-02 18:29:00 -0300

    adding support for transparent mode

--- src/Providers/MercadoPagoServiceProvider.php
+++ src/Providers/MercadoPagoServiceProvider.php
@@ -7,9 +7,13 @@ use SantiGraviano\LaravelMercadoPago\MP;
 
 class MercadoPagoServiceProvider extends ServiceProvider 
 {
+	protected $mp_app_mode;
 
 	protected $mp_app_id;
 	protected $mp_app_secret;
+
+	protected $mp_app_public_key;
+	protected $mp_app_access_token;
 
 	public function boot()
 	{
@@ -18,12 +22,19 @@ class MercadoPagoServiceProvider extends ServiceProvider
 
 		$this->mp_app_id     = config('mercadopago.app_id');
 		$this->mp_app_secret = config('mercadopago.app_secret');
+
+		$this->mp_app_public_key = config('mercadopago.app_public_key');
+		$this->mp_app_access_token = config('mercadopago.app_access_token');
 	}
 
 	public function register()
 	{
 		$this->app->singleton('MP', function(){
-			return new MP($this->mp_app_id, $this->mp_app_secret);
+			if ($this->mp_app_mode == 'transparent') {
+				return new MP($this->mp_app_access_token);
+			} else {
+				return new MP($this->mp_app_id, $this->mp_app_secret);
+			}
 		});
 	}
 }
